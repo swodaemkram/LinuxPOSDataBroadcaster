df6861aab29ae90d2dd3bc0046854ecf6f4c7707
mark <mark@mark-desktop>
2018-10-14 15:46:53 -0400

    Setting Up Includes

--- src/LinuxPOSDataBroadcaster.c
+++ src/LinuxPOSDataBroadcaster.c
@@ -10,6 +10,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/uio.h>
+
+
 
 int main(void) {
 	puts("Mark is Cool"); /* prints !!!Hello World!!! */
