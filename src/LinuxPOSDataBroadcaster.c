a9a53289bbf099abd21a15c5370b579ff8a970c6
mark <mark@mark-desktop>
2018-10-15 20:59:01 -0400

    Making Progress

--- src/LinuxPOSDataBroadcaster.c
+++ src/LinuxPOSDataBroadcaster.c
@@ -8,85 +8,67 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/uio.h>
-#include <sys/types.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <time.h>
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
 
+#define SERVER "233.0.0.1"
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to send data
 
-#define SERVER "127.0.0.1"    //Local Host I.P.
-#define PORT 10002          //Port to use for testing
-
-
-int main( int argc, char *argv[])
+void die(char *s)
 {
-   char buf[512];
-   int clientToServer_socket;
-   unsigned int fromLength;
-   struct sockaddr_in Remote_Address, Server_Address;
-   struct hostent *hostPointer;
-   int message, checkCall;
-
-
-   int broadcastOn = 1;
-   int broadcastOff = 0;
-   char *broadcastMessage;
-
-   broadcastMessage = "Mark is Cool";
-
-   printf("Transmitted %s\n", broadcastMessage);
-
-   /*Create client socket*/
-   clientToServer_socket=socket(AF_INET, SOCK_DGRAM, 0);
-   if (clientToServer_socket==-1)
-    perror("Error: client socket not created");
-
-   /*Fill in client's sockaddr_in */
-   bzero(&Remote_Address, sizeof(Remote_Address));
-   Remote_Address.sin_family=AF_INET;
-   hostPointer=gethostbyname(argv[1]);
-   memcpy((unsigned char * ) &Remote_Address.sin_addr, (unsigned char *) hostPointer -> h_addr, hostPointer -> h_length);
-   Remote_Address.sin_port=htons(atoi(argv[2]));
-
-   checkCall = setsockopt(clientToServer_socket, SOL_SOCKET, SO_BROADCAST, &broadcastOn, 4);
-   if(checkCall == -1)
-    perror("Error: setsockopt call failed");
-
-   Remote_Address.sin_addr.s_addr|=htonl(0x1ff);
-
-   checkCall = setsockopt(clientToServer_socket, SOL_SOCKET, SO_BROADCAST, &broadcastOff, 4);
-   if(checkCall == -1)
-    perror("Error: Second setsockopt call failed");
-
-   int broadcastMessageLen = strlen(broadcastMessage) + 1;
-
-   printf("Message test %d\n", broadcastMessageLen);
-
-
-   message = sendto(clientToServer_socket, broadcastMessage, broadcastMessageLen, 0, (struct sockaddr *) &Remote_Address, sizeof(Remote_Address));
-   if (message ==-1)
-    perror("Error: sendto call failed");
-
-   while(1)
-   {
-      fromLength = sizeof(Server_Address);
-      message = recvfrom(clientToServer_socket, buf, 512, 0, (struct sockaddr *) &Server_Address, &fromLength);
-      if (message ==-1)
-     perror("Error: recvfrom call failed");
-
-      printf("SERVER: read %d bytes from IP %s(%s)\n", message, inet_ntoa(Server_Address.sin_addr), buf);
-   }
-   close(clientToServer_socket);
-
+    perror(s);
+    exit(1);
 }
 
+int main(void)
+{
+    struct sockaddr_in si_other;
+    int s, i, slen=sizeof(si_other);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
+
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0)
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+
+    while(1)
+    {
+        printf("Enter message : ");
+        gets(message);
+
+        //send the message
+        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        {
+            die("sendto()");
+        }
+
+        //receive a reply and print it
+        //clear the buffer by filling null, it might have previously received data
+        memset(buf,'\0', BUFLEN);
+        //try to receive some data, this is a blocking call
+        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
+        {
+            die("recvfrom()");
+        }
+
+        puts(buf);
+    }
+
+    close(s);
+    return 0;
+}
